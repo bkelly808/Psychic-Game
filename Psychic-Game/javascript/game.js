6fe5eea10a8764a850c8972d94130b793a00f78d
bkelly808 <50977430+bkelly808@users.noreply.github.com>
2019-08-11 21:26:28 -0700

    Update game.js

--- Psychic-Game/javascript/game.js
+++ Psychic-Game/javascript/game.js
@@ -1,56 +1,44 @@
-//*Put the following text on your page:
-    //Guess what letter I'm thinking of
-    //Wins: (# of times the user has guessed the letter correctly)
-    //Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-    //Guesses Left: (# of guesses left. This will update)
-    //Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-    //When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-    //When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     var wins = 0;
     var losses = 0;
     var guessesLeft = 10;
     var guessesSofar = [];
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-    var directionsText = document.getElementById("directions-text");
-    var userChoiceText = document.getElementById("userchoice-text");
-    var computerChoiceText = document.getElementById("computerchoice-text");
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
-    var tiesText = document.getElementById("guessesLeft-text");
-    var tiesText = document.getElementById("guessesSofar-text");
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//displays Guesses so far and guesses Left (**Not sure why i am getting an error afer "guessesLeft"**)
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML ="Guesses Left: " + guessesLeft;
+}
+function guessesSofar() {
+	document.querySelector("#guessesSofar").innerHTML = "Your Guesses so far: " + guessesSofar.join(' ');
+}
+countGuessesLeft();
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+
+}
+    
+      //make letters lowercase
+      var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
     // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-    
-      // Determines which key was pressed.
-      var userGuess = event.key;
-
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-      // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-
-        if (userGuess === computerGuess) {
-          wins++;
-          attempts = 10;
-          guessesSofar = [];
-        }
-        jsPsychic();
-        if (attempts == 0) {
-          losses++;
-          guessesSofar = []
-          attempts = 10;
-        }
+      document.onkeyup = function (event) {
         
-        $(':reset');
+        // Determines which key was pressed.
+      va          wins++;
+          document.querySelector("#wins").innerHTML = "Wins: " + wins;
+          restart();
+        } 
 
-        // Display the user and computer guesses, and wins/losses/ties.
-        userChoiceText.textContent = "You chose: " + userChoice;
-        computerChoiceText.textContent = "The computer chose: " + computerChoice;
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        guessesLeftText.textContent = "guesses Left" + guessesLeft;
-        guessesSofarText.textContent = "guesses so far" + guessesSofar;
-      }
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#losses").innerHTML = "Losses: " + losses;
+		restart();
+	};r userGuess = event.key;
+  
+         // This logic determines the outcome of the game (win/loss), and increments the appropriate number
+        if (userGuess === computerGuess){
